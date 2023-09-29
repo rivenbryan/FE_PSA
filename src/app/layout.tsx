16129a2293b284586ff408834c9eb7f0c9a53088
Bryan Tay <rivenbryan@gmail.com>
2023-09-29 22:59:25 +0800

    added container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Container from "@/components/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        </body>
+        <Navbar />
+        <Container>{children}</Container>
+      </body>
     </html>
   );
 }
