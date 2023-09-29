817b9b75a4fc1c636b64af60774bdaa15351c2db
digudiguding <98702501+digudiguding@users.noreply.github.com>
2023-09-30 03:27:11 +0800

    prepare for api call

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,9 +1,30 @@
+import react, { useState } from "react";
 import { tempProducts } from "~/tempData";
+import { MarketPlaceProps } from "@/components/marketplace";
 import { Marketplace } from "@/components/marketplace";
 
 export default async function StoresCategoryPage() {
-    try {
-        
-    }
-  return <Marketplace items={tempProducts} />;
+  let items: MarketPlaceProps[] = [];
+
+  try {
+    // pull from database
+    //items = (await )
+  } catch (err) {
+    console.error(err);
+  }
+  return (
+    <div className="mx-auto">
+      <div className="bg-[url('../../public/psaBanner_containers.jpg')] bg-right-top text-custom-1 text-center py-4 sm:py-10 sm:py-20 h-[450px] sm:h-[520px]">
+        <h1 className="text-black uppercase text-3xl sm:text-6xl">
+          Marketplace
+        </h1>
+        <p className="text-black text-sm sm:text-base mx-auto px-8 sm:max-w-[50%] my-10">
+          Find your desired container sharing partners here. Filter by
+          locations, category and time then chat the leasing party!
+        </p>
+      </div>
+
+      <Marketplace items={tempProducts} />
+    </div>
+  );
 }
--- src/components/marketplace.tsx
+++ src/components/marketplace.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import react, { useState } from "react";
 
 type Item = {
   _id: string;
@@ -22,7 +22,7 @@ type Item = {
     };
   };
 };
-type MarketPlaceProps = {
+export type MarketPlaceProps = {
   items: Item[];
 };
 
@@ -31,22 +31,13 @@ const containerImage = "containerImage.png";
 export function Marketplace({ items }: MarketPlaceProps) {
   return (
     <div className="mx-auto">
-      <div className="bg-black text-custom-1 text-center py-4 sm:py-10 sm:py-20 h-[450px] sm:h-[520px]">
-        <h1 className="text-white uppercase text-3xl sm:text-6xl">
-          Marketplace
-        </h1>
-        <p className="text-white text-sm sm:text-base mx-auto px-8 sm:max-w-[50%] my-10">
-          Find your desired container sharing partners here. Filter by
-          locations, category and time then chat the leasing party!
-        </p>
-      </div>
       {items.length ? (
         <div className="full-w overflow-hidden mx-auto text-center mt-[-200px] sm:mt-[-130px] px-10">
           <ul className="grid sm:grid-cols-3 gap-8 grid-flow-row">
             {items.map((item) => (
               <li key={item._id} className="relative">
                 <a href={`/container-listing/${item.slug}`}>
-                  <div className="bg-purple-300 h-auto max-w-full">
+                  <div className="bg-black h-96 max-w-full flex justify-center items-center">
                     <img
                       className="mx-auto w-72 h-64"
                       src={containerImage}
